f1d1db8bf57a47d727c47c8bdf9fc9e0af516ba1
AveryanAlex <alex@averyan.ru>
2022-09-10 17:01:03 +0300

    Whale keys

--- secrets.nix
+++ secrets.nix
@@ -3,7 +3,8 @@ let
   users = [ alex ];
 
   alligator = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE7y1Vw/aeF69RDccAB2BB1IATUvVEQ7sIgAO5fUZKyC";
-  systems = [ alligator ];
+  whale = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtyxNV8IPSMHudJrMbemcK82LosU9tdNDV2rhf0Z9ps";
+  systems = [ alligator whale ];
 in
 {
   "secrets/passwords/alex.age".publicKeys = users ++ systems;
