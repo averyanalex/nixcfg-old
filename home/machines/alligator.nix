1d3ab70a02843983351460e8a39b8b81ce852343
AveryanAlex <alex@averyan.ru>
2022-09-11 21:01:47 +0300

    Disable GUI on alligator

--- home/machines/alligator.nix
+++ home/machines/alligator.nix
@@ -1,8 +1,8 @@
 {
   imports = [
     ../common.nix
-    ../gui
+    # ../gui
   ];
 
-  wayland.windowManager.sway.config.output."DP-1".mode = "3440x1440@144Hz";
+  # wayland.windowManager.sway.config.output."DP-1".mode = "3440x1440@144Hz";
 }
